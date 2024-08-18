d4ec025b5e6fb1f0751817fe1bc961dc2fcf546d
Dharshna202 <dharshnakumari02@gmail.com>
2024-08-18 11:20:55 +0530

    Add files via upload

---
+++ src/main/java/expensetracker/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package expensetracker.repositories;
+
+import expensetracker.domain.User;
+import expensetracker.exceptions.EtAuthException;
+
+public interface UserRepository {
+
+    Integer create(String firstName, String lastName, String email, String password) throws EtAuthException;
+
+    User findByEmailAndPassword(String email, String password) throws EtAuthException;
+
+    Integer getCountByEmail(String email);
+
+    User findById(Integer userId);
+
+}
